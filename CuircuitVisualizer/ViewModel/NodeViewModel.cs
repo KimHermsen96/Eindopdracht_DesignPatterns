0403bf97888ddc0ffd094ce80f94be2aad4c4248
KimHermsen96 <kimhermsen@hotmail.nl>
2020-06-13 17:40:07 +0200

    DeletedGraphviz folder
    DeletedGraphviz folder

--- CuircuitVisualizer/ViewModel/NodeViewModel.cs
+++ CuircuitVisualizer/ViewModel/NodeViewModel.cs
@@ -1,11 +1,6 @@
 ﻿using CircuitLogic.models.Nodes;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace CuircuitVisualizer.ViewModel
